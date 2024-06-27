dc68aa5b2ee63191fdbaed0ecb3eabc145a531f9
ChrisSmithArt <chrissmithart@proton.me>
2024-06-27 14:24:00 -0500

    added seed information from park asset csv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,32 +9,32 @@
 #   end
 
 require "csv"
-park_csv = Rails.root.join('db/Parks_and_Open_Space_20240626.csv')
-park_data = File.read(park_csv)
-parks = CSV.parse(park_data, headers: true, encoding: 'utf-8')
+# park_csv = Rails.root.join('db/Parks_and_Open_Space_20240626.csv')
+# park_data = File.read(park_csv)
+# parks = CSV.parse(park_data, headers: true, encoding: 'utf-8')
 
-parks.each do |park|
-  Park.create(
-    park_id: park['Park ID'].to_i,
-    park_name: park['Park Name'],
-    park_description: park['Location Description'],
-    park_neighbourhood: park['Neighbourhood'],
-    park_category: park['Park Category'],
-    total_area: park['Total Area in Hectares'].to_d,
-    park_location: park['Location'],
-  )
-end
-
-# parkasset_csv = Rails.root.join('db/Park_Asset_Inventory_20240626.csv')
-# parkasset_data = File.read(parkasset_csv)
-# parkassets = CSV.parse(parkasset_data, headers: true, encoding: 'utf-8')
-
-# parkassets.each do |parkasset|
-#   ParkAsset.create(
-#     id: parkasset['Asset ID'].to_i,
-#     park_id: parkasset['Park ID'].to_i,
-#     assetclass: parkasset['Asset Class'],
-#     assetsize: parkasset['Asset Size'],
-#     assettype: parkasset['Asset Type'],
+# parks.each do |park|
+#   Park.create(
+#     park_id: park['Park ID'].to_i,
+#     park_name: park['Park Name'],
+#     park_description: park['Location Description'],
+#     park_neighbourhood: park['Neighbourhood'],
+#     park_category: park['Park Category'],
+#     total_area: park['Total Area in Hectares'].to_d,
+#     park_location: park['Location'],
 #   )
 # end
+
+assets_csv = Rails.root.join('db/Park_Asset_Inventory_20240626.csv')
+assets_data = File.read(assets_csv)
+assets = CSV.parse(assets_data, headers: true, encoding: 'utf-8')
+
+assets.each do |asset|
+  ParkAsset.create(
+    asset_id: asset['Asset ID'].to_i,
+    park_id: asset['Park ID'].to_i,
+    asset_class: asset['Asset Class'],
+    asset_size: asset['Asset Size'],
+    asset_type: asset['Asset Type'],
+  )
+end
